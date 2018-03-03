fd275cad34cb64fbda0cceae8de2e0593e5819fe
Jaspreet Singh <erjaspreet57@gmail.com>
2018-03-03 13:01:56 +0530

    background color scroll added

--- src/js/default.js
+++ src/js/default.js
@@ -1,8 +1,37 @@
 // Default JavaScript Functions and Initiations
 $(document).ready(function() {
-
-  $(document).ready(function(){
+    /*
+    * sticky 
+    * */
     $("#sticky-navbar").sticky({topSpacing:0, responsiveWidth: true, zIndex: 99999999});
-  });
-  
-}); // end document ready
+
+    /* 
+    * background color scroll
+    * */
+    $(window).scroll(function() {
+      var $window = $(window),
+          $body = $('body'),
+          $panel = $('.color-panel');
+      
+      var scroll = $window.scrollTop() + ($window.height() / 3);  // Change 33% earlier than scroll position so colour is there when you arrive.
+     
+      $panel.each(function () {
+        var $this = $(this);
+        
+        // if position is within range of this panel.
+        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+        // Remember we set the scroll to 33% earlier in scroll var.
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+              
+          // Remove all classes on body with color-
+          $body.removeClass(function (index, css) {
+            return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+          });
+           
+          // Add class of currently active div
+          $body.addClass('color-' + $(this).data('color'));
+        }
+      });    
+      
+    }).scroll();
+});
